05bae4a5b2e090fe9f4e54007eccfef57f2f58fa
vagrant <vagrant@homestead>
2016-11-20 18:38:01 +0000

    Added layout and styles to the home page

---
+++ resources/views/layouts/app.blade.php
@@ -0,0 +1,17 @@
+<!DOCTYPE HTML>
+<html lang="en-gb">
+	<head>
+		@yield('head')
+
+		<!-- Latest compiled and minified CSS -->
+		<link rel="stylesheet" href="https://maxcdn.bootstrapcdn.com/bootstrap/3.3.7/css/bootstrap.min.css" integrity="sha384-BVYiiSIFeK1dGmJRAkycuHAHRg32OmUcww7on3RYdg4Va+PmSTsz/K68vbdEjh4u" crossorigin="anonymous">
+
+		<link rel="stylesheet" href="css/app.css" />
+	</head>
+	<body>
+		@yield('content')
+
+		<!-- Latest compiled and minified JavaScript -->
+		<script src="https://maxcdn.bootstrapcdn.com/bootstrap/3.3.7/js/bootstrap.min.js" integrity="sha384-Tc5IQib027qvyjSMfHjOMaLkfuWVxZxUPnCJA7l2mCWNIpG9mGCD8wGNIcPD7Txa" crossorigin="anonymous"></script>
+	</body>
+</html>
--- resources/views/welcome.blade.php
+++ resources/views/welcome.blade.php
@@ -1,95 +1,107 @@
-<!DOCTYPE html>
-<html lang="en">
-    <head>
-        <meta charset="utf-8">
-        <meta http-equiv="X-UA-Compatible" content="IE=edge">
-        <meta name="viewport" content="width=device-width, initial-scale=1">
+@extends('layouts.app')
 
-        <title>Laravel</title>
+@section('head')
+    <title>Hi Santa - The Secret Santa Generator</title>
+    <meta name="description" value="Got a secret santa group coming up? Hi Santa will automatically work out your group">
+@endsection
 
-        <!-- Fonts -->
-        <link href="https://fonts.googleapis.com/css?family=Raleway:100,600" rel="stylesheet" type="text/css">
+@section('content')
+    <div class="container">
+        <div class="row">
+            <div class="col-md-12 ">
+                <img src="images/santa.png" alt="Santa" width="241" height="343" />
 
-        <!-- Styles -->
-        <style>
-            html, body {
-                background-color: #fff;
-                color: #636b6f;
-                font-family: 'Raleway', sans-serif;
-                font-weight: 100;
-                height: 100vh;
-                margin: 0;
-            }
-
-            .full-height {
-                height: 100vh;
-            }
-
-            .flex-center {
-                align-items: center;
-                display: flex;
-                justify-content: center;
-            }
-
-            .position-ref {
-                position: relative;
-            }
-
-            .top-right {
-                position: absolute;
-                right: 10px;
-                top: 18px;
-            }
-
-            .content {
-                text-align: center;
-            }
-
-            .title {
-                font-size: 84px;
-            }
-
-            .links > a {
-                color: #636b6f;
-                padding: 0 25px;
-                font-size: 12px;
-                font-weight: 600;
-                letter-spacing: .1rem;
-                text-decoration: none;
-                text-transform: uppercase;
-            }
-
-            .m-b-md {
-                margin-bottom: 30px;
-            }
-        </style>
-    </head>
-    <body>
-        <div class="flex-center position-ref full-height">
-            @if (Route::has('login'))
-                <div class="top-right links">
-                    @if (Auth::check())
-                        <a href="{{ url('/home') }}">Home</a>
-                    @else
-                        <a href="{{ url('/login') }}">Login</a>
-                        <a href="{{ url('/register') }}">Register</a>
-                    @endif
-                </div>
-            @endif
-
-            <div class="content">
-                <div class="title m-b-md">
-                    Laravel
+                <div id="santa-intro">
+                    Make Me Santa
                 </div>
 
-                <div class="links">
-                    <a href="https://laravel.com/docs">Documentation</a>
-                    <a href="https://laracasts.com">Laracasts</a>
-                    <a href="https://laravel-news.com">News</a>
-                    <a href="https://forge.laravel.com">Forge</a>
-                    <a href="https://github.com/laravel/laravel">GitHub</a>
-                </div>
+                <form id="santa-form" method="post" action="/santa">
+                    <div class="row">
+                        <div class="col-md-12 form-heading">
+                            About you...
+                        </div>
+                    </div>
+
+                    <div class="row">
+                        <div class="col-md-6 form-label">
+                            <label for="primary-name">Your Name:</label>
+                        </div>
+                        <div class="col-md-6 form-label">
+                            <label for="primary-email">Your Email:</label>
+                        </div>
+                    </div>
+                    <div class="row">
+                        <div class="col-md-6 form-input">
+                            <input type="text" name="primary_name" class="form-field" />
+                        </div>
+                        <div class="col-md-6 form-input">
+                            <input type="text" name="primary_email" class="form-field" />
+                        </div>
+                    </div>
+
+                    <div class="row">
+                        <div class="col-md-12 form-heading">
+                            Your friends...
+                        </div>
+                    </div>
+
+                    <div class="row">
+                        <div class="col-md-6 form-label">
+                            <label for="primary-name">Their Name:</label>
+                        </div>
+                        <div class="col-md-6 form-label">
+                            <label for="primary-email">Their Email:</label>
+                        </div>
+                    </div>
+
+                    <div class="row">
+                        <div class="col-md-6 form-input">
+                            <input type="text" name="participant_name_01" class="form-field" />
+                        </div>
+                        <div class="col-md-6 form-input">
+                            <input type="text" name="participant_email_01" class="form-field" />
+                        </div>
+                    </div>
+                    <div class="row">
+                        <div class="col-md-6 form-input">
+                            <input type="text" name="participant_name_02" class="form-field" />
+                        </div>
+                        <div class="col-md-6 form-input">
+                            <input type="text" name="participant_email_02" class="form-field" />
+                        </div>
+                    </div>
+                    <div class="row">
+                        <div class="col-md-6 form-input">
+                            <input type="text" name="participant_name_03" class="form-field" />
+                        </div>
+                        <div class="col-md-6 form-input">
+                            <input type="text" name="participant_email_03" class="form-field" />
+                        </div>
+                    </div>
+                    <div class="row">
+                        <div class="col-md-6 form-input">
+                            <input type="text" name="participant_name_04" class="form-field" />
+                        </div>
+                        <div class="col-md-6 form-input">
+                            <input type="text" name="participant_email_04" class="form-field" />
+                        </div>
+                    </div>
+                    <div class="row">
+                        <div class="col-md-6 form-input">
+                            <input type="text" name="participant_name_05" class="form-field" />
+                        </div>
+                        <div class="col-md-6 form-input">
+                            <input type="text" name="participant_email_05" class="form-field" />
+                        </div>
+                    </div>
+
+                    <div class="row">
+                        <div class="col-md-12 form-submit">
+                            <input type="submit" value="Submit" class="btn btn-primary" />
+                        </div>
+                    </div>
+                </form>
             </div>
         </div>
-    </body>
-</html>
+    </div>
+@endsection
