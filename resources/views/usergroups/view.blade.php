f102c99976411a056f293c7601bc02a6f373daae
stingrayyy <leepetryk+github@gmail.com>
2017-01-12 23:36:29 +0000

    Some basic styling for the view page

--- resources/views/usergroups/view.blade.php
+++ resources/views/usergroups/view.blade.php
@@ -6,12 +6,19 @@
 @endsection
 
 @section('content')
-	@if (!$hash_found)
-		<p>A match could not be found, sorry!</p>
-		<p>Please check your mailbox for an email from us and try again.</p>
-		<p>Alternatively - <a href="/">set up a new group</a> using Hi Santa.</p>
-	@else
-		<p>Hi there {{ $santa }}</p>
-		<p>You've been selected to be santa for {{ $recipient }}. Please remember to buy a present before the group meet up.</p>
-	@endif
+	<div class="container">
+		<div class="row">
+			<div class="col-md-12" id="view-content">
+				@if (!$hash_found)
+					<h1>Sorry!</h1>
+					<p>A match could not be found.</p>
+					<p>Please check your mailbox for an email from us and try again.</p>
+					<p>Alternatively - <a href="/">set up a new group</a> using Hi Santa.</p>
+				@else
+					<h1>Hi {{ $santa }}!</h1>
+					<p>You've been selected to be santa for {{ $recipient }}. Please remember to buy a present before the group meet up.</p>
+				@endif
+			</div>
+		</div>
+	</div>
 @endsection
